831d0a1c4e78cb2e2d603f6dbee5521a3a74ded0
lbai <bailin_iceandfire@outlook.com>
2021-09-16 11:03:19 -0500

    edit load_fTable_merged.csv.py

--- Tool/load_fTable_merged.csv.py
+++ Tool/load_fTable_merged.csv.py
@@ -1,9 +1,16 @@
 import pandas as pd
 import pickle
-table = pd.read_csv('F:/results/fTable_merged.csv')
-Cell_ID_centerXY={}
-for index, row in table.iterrows():
-    Cell_ID_centerXY[int(row['ID'])]=[int(row['centroid_x']), int(row['centroid_y'])]
-f = open("Cell_ID_centerXY.pkl","wb")
-pickle.dump(Cell_ID_centerXY,f)
-f.close()
+def csv2pkl(fTable_merged_address,Cell_ID_centerXY_address):
+    table = pd.read_csv(fTable_merged_address)
+    Cell_ID_centerXY={}
+    for index, row in table.iterrows():
+        Cell_ID_centerXY[int(row['ID'])]=[int(row['centroid_x']), int(row['centroid_y'])]
+    f = open(Cell_ID_centerXY_address,"wb")
+    pickle.dump(Cell_ID_centerXY,f)
+    f.close()
+def load_Cell_ID_centerXY_pkl(Cell_ID_centerXY_address='.Tool/results/Cell_ID_centerXY.pkl'):
+    f = open(Cell_ID_centerXY_address,"rb")
+    print(pickle.load(f))
+    f.close()
+if __name__ == "__main__":
+    csv2pkl('D:/fTable_merged.csv','.Tool/results/Cell_ID_centerXY.pkl')
